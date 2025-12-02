f988dacc2ad53826e1fb20fe0361001b9afada23
“philippe <philippe.mail.perso@gmail.com>
2025-12-02 17:11:01 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'One Page Grayscale',
     'One Page Grayscale Bootstrap theme for Dotclear',
     'Start Bootstrap and Philippe aka amalgame',
-    '2.0',
+    '2.1',
     [
         'requires'          => [['core', '2.36']],
         'standalone_config' => true,
